381d336679ba15c26c2c04cfaf9a9441812287dd
krishna saparia <krishnasaparia43045@gmail.com>
2020-12-11 14:23:14 -0500

    Created Ingredient shared model and add shopping list section

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RecipesModel} from '../recipes.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: RecipesModel[] = [
+    new RecipesModel('A test recipe', 'Simply test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.delish.com%2Fcooking%2Frecipe-ideas%2Fa19665622%2Feasy-chicken-fajitas-recipe%2F&psig=AOvVaw2CqmgMiNPS5FT_ksQcNqqX&ust=1607287113088000&source=images&cd=vfe&ved=2ahUKEwiFuNic2bftAhUiA1kFHdOHBX4QjRx6BAgAEAc')
+  ];
   constructor() { }
 
   ngOnInit(): void {
