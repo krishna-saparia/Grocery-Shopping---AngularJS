9325a2a41ff21a0c00153fdc024839bb6e77d374
krishna saparia <krishnasaparia43045@gmail.com>
2021-03-13 14:00:49 -0500

    Made minor changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { EventEmitter } from '@angular/core';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<RecipesModel>();
   recipes: RecipesModel[] = [
-    new RecipesModel('A test recipe', 'Simply test', '19105402-food-ingredients-and-recipe-paper-on-white-background')
+    new RecipesModel('A test recipe', 'Simply test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
   constructor() { }
 
